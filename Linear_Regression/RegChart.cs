1b6f0ea8f5f1d334011f8594b73df12401503f17
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-07-08 17:51:40 +0200

    Added List<Tuple<>> functions

--- Linear_Regression/RegChart.cs
+++ Linear_Regression/RegChart.cs
@@ -11,18 +11,29 @@ namespace Linear_Regression
 {
     public class RegChart 
     {
+        private Dictionary<double, double> dictionary;
         private List<Tuple<double, double>> tuples;
+
         public KeyValuePair<double, double> Coefficients { get; set; }
         private double bCoefficients;
         private double aCoefficients;
         
-      
+        public RegChart(Dictionary<double, double> dictionary)
+        {
+            this.dictionary = dictionary;
+            Coefficients = GetCoefficientsByDictionary();
+            aCoefficients = Coefficients.Key;
+            bCoefficients = Coefficients.Value;
+            
+        }
+
         public RegChart(List<Tuple<double, double>> tuples)
         {
             this.tuples = tuples;
             Coefficients = GetCoefficientsByTuple();
             aCoefficients = Coefficients.Key;
             bCoefficients = Coefficients.Value;
+
         }
         private KeyValuePair<double, double> GetCoefficientsByTuple()
         {
@@ -34,7 +45,16 @@ namespace Linear_Regression
             var a = meanY - (b * meanX);
             return new KeyValuePair<double, double>(a, b);
         }
- 
+        private  KeyValuePair<double,double> GetCoefficientsByDictionary()
+        {   
+            double meanX = dictionary.Select(x => x.Key).Sum() / dictionary.Count;
+            // srednia y
+            double meanY = dictionary.Select(x => x.Value).Sum() / dictionary.Count;
+            var b = dictionary.Sum(x => (x.Value - meanY) * (x.Key - meanX)) /
+                    dictionary.Sum(x => (x.Key - meanX) * (x.Key - meanX));
+            var a = meanY - (b * meanX);
+            return new KeyValuePair<double,double>( a, b);
+        }
         public void PrintLineChartTuple(string SeriesName, ref Chart chart)
         {
             foreach (var key in tuples)
@@ -42,10 +62,23 @@ namespace Linear_Regression
                 chart.Series[SeriesName].Points.AddXY(key.Item1, Function(key.Item1, bCoefficients, aCoefficients));
             }
         }
+        public void PrintLineChart( string SeriesName, ref Chart chart)
+        {
+            Chart c = chart;
+            chart.Series[0].IsVisibleInLegend = false;
+            foreach (var key in dictionary)
+            {
+               chart.Series[SeriesName].Points.AddXY(key.Key, Function(key.Key, bCoefficients, aCoefficients));
+            }
+        }
 
         private double Function(double key, double bCoefficients, double aCoefficients)
         {
             return key * bCoefficients + aCoefficients  ;
-        } 
+        }
+
+     
+
+      
     }
 }
